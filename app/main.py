5b9ca6e86f5431e41409997cfc72eb40c29e04f5
Guido Pereira <stereopt@gmail.com>
2023-08-21 10:57:12 +0100

    feat(app): random model from array

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
-def start():
+from .models import get_random_model
+
+
+def main():
     print("[Stereo.AI]")
+    print(f"Random Model: '{get_random_model()}'")
---
+++ app/models/main.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_random_model():
+    models = [
+        "Realistic Vision V5.1",
+        "DreamShaper 8",
+        "Perfect World v5",
+        "MeinaHentai",
+    ]
+    random_model = random.choice(models)
+
+    return random_model
