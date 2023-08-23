c2395c125c48fec92b0caacc0ad989f4a137e120
Guido Pereira <stereopt@gmail.com>
2023-08-23 09:45:17 +0100

    feat(app): added gradio

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+## RUN: gradio app/main.py demo.app
+
+import gradio as gr
+
 from .models import get_random_model
 
 
-def main():
-    print("[Stereo.AI]")
-    print(f"Random Model: '{get_random_model()}'")
+def greet(name):
+    return f"Hello {name}!"
+
+
+with gr.Blocks() as demo:
+    text = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+
+if __name__ == "__main__":
+    demo.launch()
