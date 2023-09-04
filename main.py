f6d32c695403b7bff71b07f1fbc758d68e03882c
Guido Pereira <stereopt@gmail.com>
2023-09-05 00:05:05 +0100

    feat(app): architecture

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import uvicorn
-
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from .routes import generation
+from routes import generation
 
 
 app = FastAPI()
@@ -17,8 +15,3 @@ def docs():
 
 
 app.include_router(generation.router)
-
-
-def start():
-    """Launched with `poetry run dev` at root level"""
-    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
