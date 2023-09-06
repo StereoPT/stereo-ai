47879b000e4f1bf5351dfde68ad3b15bd6dbd4a3
Guido Pereira <stereopt@gmail.com>
2023-09-07 00:49:03 +0100

    chore(app): cleanup

--- utils/models.py
+++ utils/models.py
@@ -8,3 +8,10 @@ def get_model():
 
     random_model = random.choice(models)
     return random_model
+
+
+# def get_models_from_folder(models_directory):
+#     models_pattern = path.join(models_directory, "*.safetensors")
+#     model_file_paths = glob.glob(models_pattern)
+#     model_files = [Path(model_path).stem for model_path in model_file_paths]
+#     return model_files
