9eb6b06751b16467233696eb5881c219a0ca4909
Guido Pereira <stereopt@gmail.com>
2023-09-07 18:19:47 +0100

    feat(app): keyword amount

--- generations/main.py
+++ generations/main.py
@@ -5,7 +5,7 @@ from utils.models import get_model
 class Generation:
     def __init__(self) -> None:
         self.model = get_model()
-        self.keywords = get_keywords(15)
+        self.keywords = get_keywords()
 
     def generate(self) -> None:
         return {"model": self.model, "keywords": self.keywords}
--- utils/keywords.py
+++ utils/keywords.py
@@ -2,13 +2,15 @@ import random
 from .file import get_json_from_file
 
 
-def get_keywords(amount: int):
+def get_keywords():
     keywords = []
     positive_prompts = get_json_from_file("./data/positive_prompts.json")
 
     for prompt in positive_prompts:
-        prompt_keywords = [p.strip() for p in prompt.split(",")]
+        split_prompt = prompt.split(",")
+        prompt_keywords = [p.strip() for p in split_prompt]
         keywords.extend(prompt_keywords)
 
-    random_keywords = random.sample(keywords, amount)
+    keyword_median = len(keywords) // len(positive_prompts)
+    random_keywords = random.sample(keywords, keyword_median)
     return random_keywords
