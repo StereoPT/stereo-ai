c8e0ca26e53494c6f78c7ece8e58e8e17ae2780b
Guido Pereira <stereopt@gmail.com>
2023-10-18 18:16:28 +0100

    fix: shuffle keywords

--- src/controllers/generations.controller.js
+++ src/controllers/generations.controller.js
@@ -4,14 +4,14 @@ import random from 'random';
 export const generate = async (req, res, next) => {
   try {
     const randomModel = random.choice(models);
-    const shuffledKeywords = keywords.sort(() => random.normal());
+    const shuffledKeywords = keywords.sort(() => 0.5 - random.float());
     const randomKeywords = shuffledKeywords.slice(0, 20);
     const prompt = randomKeywords.join(', ');
 
     return res.json({
       model: randomModel,
-      keywords: randomKeywords,
       prompt: prompt,
+      keywords: randomKeywords,
     });
   } catch (error) {
     next(error);
