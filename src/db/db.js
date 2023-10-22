f53709800390b33c3ac320dde46688f30864ba96
Guido Pereira <stereopt@gmail.com>
2023-10-22 22:15:04 +0100

    fix: sync

--- src/db/db.js
+++ src/db/db.js
@@ -13,6 +13,7 @@ export const sequelize = new Sequelize(
 
 try {
   await sequelize.authenticate();
+  await sequelize.sync({ alter: true });
 } catch (error) {
   console.error('Unable to connect to the database:', error);
 }
