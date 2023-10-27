663754e0a5b0586258cecee43db7752be255817a
Guido Pereira <stereopt@gmail.com>
2023-10-27 09:52:21 +0100

    refactor: removed auto-incremented id from model table

--- backend/src/models/models.model.ts
+++ backend/src/models/models.model.ts
@@ -1,19 +1,17 @@
-import { DataTypes, Model as SeqModel, Optional } from 'sequelize';
+import { DataTypes, Model as SeqModel } from 'sequelize';
 import { sequelize } from '../db';
 
 interface ModelAttributes {
-  id: number;
   name: string;
   version: string;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
-export interface ModelInput extends Optional<ModelAttributes, 'id'> {}
+export interface ModelInput extends ModelAttributes {}
 export interface ModelOutput extends Required<ModelAttributes> {}
 
 export class Model extends SeqModel<ModelAttributes, ModelInput> {
-  declare id: number;
   declare name: string;
   declare version: string;
   declare createdAt?: Date;
@@ -22,29 +20,19 @@ export class Model extends SeqModel<ModelAttributes, ModelInput> {
 
 Model.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
     name: {
       type: DataTypes.STRING,
+      primaryKey: true,
       allowNull: false,
     },
     version: {
       type: DataTypes.STRING,
+      primaryKey: true,
       allowNull: false,
     },
   },
   {
     timestamps: true,
     sequelize,
-    indexes: [
-      {
-        fields: ['name', 'version'],
-        unique: true,
-      },
-    ],
   },
 );
