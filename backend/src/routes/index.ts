5d17329bebafc1ef863f325847841fafdea61efd
Guido Pereira <stereopt@gmail.com>
2023-10-28 15:19:50 +0100

    refactor: routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,6 @@
-import KeywordsRoutes from './keywords';
-import ModelRoutes from './models';
-import GenerationRoutes from './generations';
-import PromptRoutes from './prompt';
+import KeywordsRoutes from './keywords.routes';
+import ModelRoutes from './models.routes';
+import GenerationRoutes from './generations.routes';
+import PromptRoutes from './prompts.routes';
 
 export { KeywordsRoutes, ModelRoutes, GenerationRoutes, PromptRoutes };
